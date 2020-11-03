f2edef4c2d4143c14d3e905c50bc684aded9b727
hassane <hassaneseoud@gmail.com>
2020-11-03 22:22:55 +0100

    adding calculateParticipation test and commenting integration test

--- src/test/scala/com/hs/pipeline/demo/test/transformations/ContractJoinsTests.scala
+++ src/test/scala/com/hs/pipeline/demo/test/transformations/ContractJoinsTests.scala
@@ -3,7 +3,9 @@ package com.hs.pipeline.demo.test.transformations
 import com.hs.pipeline.demo.test.LsTestingBase
 import com.hs.pipeline.demo.schema.Country.joinWithCountry
 import com.hs.pipeline.demo.schema.Clients.joinWithClient
-import com.hs.pipeline.demo.transformations.ContractTransformation.createBv
+import com.hs.pipeline.demo.schema.{BusinessView, Contract}
+import com.hs.pipeline.demo.transformations.ContractTransformation
+import org.apache.spark.sql.types._
 
 class ContractJoinsTests extends LsTestingBase{
 
@@ -30,11 +32,31 @@ class ContractJoinsTests extends LsTestingBase{
 
     // output of the joinWithClient
     val joinResultDf = joinWithClient (clientDf)(contractDf)
-
-    // def assertDataFrameEquals(expected : org.apache.spark.sql.DataFrame, result : org.apache.spark.sql.DataFrame)
     assertDataFrameEquals(resultDf, joinResultDf)
   }
 
+ test("calculateParticipation"){
+
+   val resultSchema = StructType(Array(
+     StructField(Contract.Id, StringType, true),
+     StructField(Contract.Entity, StringType, true),
+     StructField(Contract.Amount, DoubleType, true),
+     StructField(BusinessView.Participation, DoubleType, true)
+   ))
+
+
+   val contractSchema = StructType(Array(
+     StructField(Contract.Id, StringType, true),
+     StructField(Contract.Entity, StringType, true),
+     StructField(Contract.Amount, DoubleType, true)
+   ))
+   val contractDf = loadTestResourcesCSV("/calculateParticipation/Contract.csv", contractSchema)
+   val resultDf = loadTestResourcesCSV("/calculateParticipation/Result.csv", resultSchema).orderBy(Contract.Id)
+   assertDataFrameEquals(resultDf, ContractTransformation.calculateParticipation(contractDf).orderBy(Contract.Id))
+
+
+ }
+  /**
 
   test("ContractCreateBvTest"){
 
@@ -49,6 +71,6 @@ class ContractJoinsTests extends LsTestingBase{
 
     // def assertDataFrameEquals(expected : org.apache.spark.sql.DataFrame, result : org.apache.spark.sql.DataFrame)
     assertDataFrameEquals(resultDf, createBvResultDf)
-  }
+  }**/
 
 }
